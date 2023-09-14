b2359748e597e5f953bce60f11c9eaf7b4dc31c6
Vittorio <alieniscomparsi@gmail.com>
2023-09-14 14:06:31 +0200

    Initial commit

---
+++ src/main/java/com/morecobblestone/MoreCobblestone.java
@@ -0,0 +1,53 @@
+package com.morecobblestone;
+
+import com.mojang.logging.LogUtils;
+import com.morecobblestone.block.ModBlock;
+import com.morecobblestone.block.ModBlockCreate;
+import com.morecobblestone.item.ModItems;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(MoreCobblestone.MODID)
+public class MoreCobblestone {
+    public static final String MODID = "morecobblestone";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MoreCobblestone() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+        ModBlock.register(modEventBus);
+
+        //Create
+        if(ModList.get().isLoaded("create")) {
+            ModBlockCreate.register(modEventBus);
+        }
+
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
---
+++ src/main/java/com/morecobblestone/item/ModItems.java
@@ -0,0 +1,14 @@
+package com.morecobblestone.item;
+
+import com.morecobblestone.MoreCobblestone;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MoreCobblestone.MODID);
+
+
+    public static void register(IEventBus eventBus) { ITEMS.register(eventBus); }
+}
